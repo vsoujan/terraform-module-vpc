1cea9fa16e672a58c9307fbcca7ecd9991943ac6
soujanvangapandu <vangapandusoujan6@gmail.com>
2023-09-15 11:03:58 +0530

    vpc

--- locals.tf
+++ locals.tf
@@ -1,15 +1,15 @@
 locals {
-  #subnet
-  public_subnet_id = [for k, v in lookup(lookup(module.subnets, "public", null), "subnet_ids", null) : v.id]
-  app_subnet_id = [for k, v in lookup(lookup(module.subnets, "app", null), "subnet_ids", null) : v.id]
-  db_subnet_id = [for k, v in lookup(lookup(module.subnets, "db", null), "subnet_ids", null) : v.id]
-  private_subnet_id= concat(local.app_subnet_id, local.db_subnet_id)
 
-  #route_table
-  public_routetable_id= [for k, v in lookup(lookup(module.subnets, "public", null), "route_table_ids", null) : v.id ]
-  app_routetable_id= [for k, v in lookup(lookup(module.subnets, "app", null), "route_table_ids", null) : v.id ]
-  db_routetable_id= [for k, v in lookup(lookup(module.subnets, "db", null), "route_table_ids", null) : v.id ]
-  private_routetable_id= concat(local.app_routetable_id, local.db_routetable_id)
+  # Subnets
+  public_subnet_ids  = [for k, v in lookup(lookup(module.subnets, "public", null), "subnet_ids", null) : v.id]
+  app_subnet_ids     = [for k, v in lookup(lookup(module.subnets, "app", null), "subnet_ids", null) : v.id]
+  db_subnet_ids      = [for k, v in lookup(lookup(module.subnets, "db", null), "subnet_ids", null) : v.id]
+  private_subnet_ids = concat(local.app_subnet_ids, local.db_subnet_ids)
 
+  # RT
+  public_route_table_ids  = [for k, v in lookup(lookup(module.subnets, "public", null), "route_table_ids", null) : v.id]
+  app_route_table_ids     = [for k, v in lookup(lookup(module.subnets, "app", null), "route_table_ids", null) : v.id]
+  db_route_table_ids      = [for k, v in lookup(lookup(module.subnets, "db", null), "route_table_ids", null) : v.id]
+  private_route_table_ids = concat(local.app_route_table_ids, local.db_route_table_ids)
 
 }
