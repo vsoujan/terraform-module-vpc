a96cc7ace9e00cb351cfd8fa09448fd10bb74ddc
soujanvangapandu <vangapandusoujan6@gmail.com>
2023-09-15 14:38:08 +0530

    vpc

--- main.tf
+++ main.tf
@@ -17,30 +17,7 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-resource "aws_route" "igw" {
-  count                  = length(local.public_route_table_ids)
-  route_table_id         = element(local.public_route_table_ids, count.index)
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
-}
 
-resource "aws_eip" "ngw" {
-  count  = length(local.public_subnet_ids)
-  domain = "vpc"
-}
-
-resource "aws_nat_gateway" "ngw" {
-  count         = length(local.public_subnet_ids)
-  allocation_id = element(aws_eip.ngw.*.id, count.index)
-  subnet_id     = element(local.public_subnet_ids, count.index)
-}
-
-resource "aws_route" "ngw" {
-  count                  = length(local.private_route_table_ids)
-  route_table_id         = element(local.private_route_table_ids, count.index)
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = element(aws_nat_gateway.ngw.*.id, count.index)
-}
 
 output "subnets" {
   value = module.subnets
