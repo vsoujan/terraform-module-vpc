56ca1ccccb93f32aec70d31757dc4757cac2b1e4
soujanvangapandu <vangapandusoujan6@gmail.com>
2023-09-16 18:01:45 +0530

    vpc

--- main.tf
+++ main.tf
@@ -80,7 +80,7 @@ resource "aws_security_group" "allow_tls" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = "0.0.0./0"
+    cidr_blocks      = ["0.0.0./0"]
   }
 
   egress {
