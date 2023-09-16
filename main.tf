e93d12c3490f470cc074be091e05a419247af6fc
soujanvangapandu <vangapandusoujan6@gmail.com>
2023-09-16 18:00:01 +0530

    vpc

--- main.tf
+++ main.tf
@@ -61,3 +61,37 @@ resource "aws_route" "default_peer" {
   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 }
 
+resource "aws_instance" "test" {
+
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  ami = "ami-03265a0778a880afb"
+  subnet_id = local.app_subnet_ids[0]
+
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = "0.0.0./0"
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
